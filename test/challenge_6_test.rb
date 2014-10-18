99addb15fa61c1cffe0057e52924dee79d20c6ca
Mike Busch <busch.michael@gmail.com>
2014-10-18 00:18:28 -0500

    more refactoring

--- test/challenge_6_test.rb
+++ test/challenge_6_test.rb
@@ -13,37 +13,24 @@ class Challenge6Test < MatasanoTest
 
   def test_break_repeating_xor_key
     b64content = File.read(File.expand_path('../challenge_6.txt', __FILE__)).chomp.gsub("\n", "")
+    contents = hex_to_bytes(base64_to_hex(b64content))
 
-    hexcontent = base64_to_hex(b64content)
-
-    contents = hex_to_bytes(hexcontent)
-
-    candidate_keysizes = 2.upto(40).map { |keysize|
+    candidate_keysizes = 2.upto(40).sort_by { |keysize|
       chunk_count = 10
-      chunk_combinations = contents.chars.take(keysize * chunk_count).each_slice(keysize).to_a.map(&:join).permutation(2).map { |a| Set.new(a) }.uniq
-      sample_count = [20, chunk_combinations.length].min
+      chunk_combinations = contents.chars.each_slice(keysize).to_a.slice(0,10).map(&:join).permutation(2).map { |a| Set.new(a) }.uniq
 
-      hd_total = chunk_combinations.sample(sample_count).reduce(0) do |hd, s|
+      hd_total = chunk_combinations.reduce(0) do |hd, s|
         hd + hamming_distance(*s.to_a)
       end
 
-      [keysize, hd_total / keysize.to_f]
-    }.sort_by(&:last).map(&:first).take(1)
-
-    p candidate_keysizes
-
-    # # p candidate_keysizes
+      hd_total / keysize.to_f
+    }.take(1)
 
     candidate_keysizes.each do |candidate_keysize|
       first, *rest = contents.chars.each_slice(candidate_keysize).to_a
-      puts '============'
-      # p first
-      # p rest.first
-      # p first.zip(rest.first).map(&:join)
-      #
-      p candidate_key = first.zip(*rest).map(&:join).map { |s| find_cipher_key(s) }.join
+      candidate_key = first.zip(*rest).map(&:join).map { |s| find_cipher_key(s) }.join
 
-      p decrypt(contents, candidate_key)
+      decrypt(contents, candidate_key)
     end
   end
 end
