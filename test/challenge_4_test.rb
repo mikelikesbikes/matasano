47d281299cd25f3c0eb94e9d25066ddf37285010
Mike Busch <busch.michael@gmail.com>
2014-10-22 13:32:20 -0500

    refactoring

--- test/challenge_4_test.rb
+++ test/challenge_4_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class Challenge4Test < Minitest::Test
   def test_find_decryption_in_file
-    strings = File.readlines(File.expand_path("../challenge-4.txt", __FILE__))
+    filename = File.expand_path("../challenge-4.txt", __FILE__)
+    strings = File.readlines(filename)
                   .map { |l| hex_to_bytes(l.chomp) }
 
     assert_equal("Now that the party is jumping\n",
